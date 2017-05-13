741136249d2585d407fb175337b6e105613d173f
ryo katsuma <katsuma@gmail.com>
2017-05-14 01:29:41 +0900

    Use bulma

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+  entry: './assets/stylesheets/app.scss',
+
+  output: {
+    path: path.resolve(__dirname, '.tmp/dist/stylesheets'),
+    filename: 'bundle.css'
+  },
+
+  resolve: {
+    modules: ['node_modules']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: "style-loader",
+            use: ["css-loader", "sass-loader?outputStyle=expanded"]
+          }
+        )
+      }
+    ]
+  },
+
+  plugins: [
+    new ExtractTextPlugin('bundle.css')
+  ]
+};
